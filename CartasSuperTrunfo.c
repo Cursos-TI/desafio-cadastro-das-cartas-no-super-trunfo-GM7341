32bbc8278b8b7a408f8873c89581dea30aaf81f7
GM7341 <gabrielmoreiranascimento7341@gmail.com>
2025-02-06 01:21:31 +0000

    Realizei a atividade no nível mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,49 +7,93 @@
 //Teste larissa
 
 int main() {
-    //váriaveis
+    // Variáveis carta 1
     char nomec[20];
     int pontosT;
     float pib, area, populacao;
     char cod1;
     int cod2;
     float pibc, densidade;
-
     double superpoder;
 
-    //Entrada de dados
+    // Variáveis carta 2
+    char nomec2[20];
+    int pontosT2;
+    float pib2, area2, populacao2;
+    char cod12;
+    int cod22;
+    float pibc2, densidade2;
+    double superpoder2;
+
+    // Entrada de dados
     printf("Digite o código da cidade (por exemplo, A01, A02, B01, B02): ");
-    scanf("%c %d",&cod1, &cod2);
+    scanf(" %c %d", &cod1, &cod2); // Adicione um espaço antes de %c
 
     printf("\nDigite o nome da cidade: ");
-    scanf("%s",&nomec);
+    scanf("%s", nomec); // Remova o &
 
     printf("\nDigite a população: ");
-    scanf("%f",&populacao);
+    scanf("%f", &populacao);
 
     printf("\nDigite a área: ");
-    scanf("%f",&area);
+    scanf("%f", &area);
 
     printf("\nDigite o PIB: ");
-    scanf("%f",&pib);
+    scanf("%f", &pib);
 
     printf("\nDigite o número de pontos turísticos: ");
-    scanf("%d",&pontosT);
+    scanf("%d", &pontosT);
 
-    //Cálculos
+    // Entrada de dados 2
+    printf("\nDigite o código da cidade (por exemplo, A01, A02, B01, B02): ");
+    scanf(" %c %d", &cod12, &cod22); // Adicione um espaço antes de %c
+
+    printf("\nDigite o nome da cidade: ");
+    scanf("%s", nomec2); // Remova o &
+
+    printf("\nDigite a população: ");
+    scanf("%f", &populacao2);
+
+    printf("\nDigite a área: ");
+    scanf("%f", &area2);
+
+    printf("\nDigite o PIB: ");
+    scanf("%f", &pib2);
+
+    printf("\nDigite o número de pontos turísticos: ");
+    scanf("%d", &pontosT2);
+
+    // Cálculos
     densidade = populacao / area;
     pibc = pib / populacao;
     superpoder = populacao + area + pib + pontosT + pibc + densidade;
 
-    //Saída de dados
-    printf("\n\nCódigo da cidade: %c%d\n", cod1, cod2);
-    printf("Nome: %s\n", nomec);
-    printf("População: %.3f\n", populacao);
-    printf("Área: %.3fKm²\n", area);
-    printf("PIB: %.3f\n", pib);
-    printf("Número de pontos turísticos %d\n", pontosT);
-    printf("Densidade populacional: %.2f\n", densidade);
-    printf("PIB per capita: %.4f\n", pibc);
-    printf("Super poder: %f", superpoder);
+    // Cálculos 2
+    densidade2 = populacao2 / area2;
+    pibc2 = pib2 / populacao2;
+    superpoder2 = populacao2 + area2 + pib2 + pontosT2 + pibc2 + densidade2;
+
+    // Saída de dados
+    printf("\n\nCódigo da cidade: %c%d / %c%d\n", cod1, cod2, cod12, cod22);
+    printf("Nome: %s / %s\n", nomec, nomec2);
+    printf("População: %.3f / %.3f\n", populacao, populacao2);
+    printf("Área: %.3f Km² / %.3f Km²\n", area, area2);
+    printf("PIB: %.3f / %.3f\n", pib, pib2);
+    printf("Número de pontos turísticos: %d / %d\n", pontosT, pontosT2);
+    printf("Densidade populacional: %.2f / %.2f\n", densidade, densidade2);
+    printf("PIB per capita: %.4f / %.4f\n", pibc, pibc2);
+    printf("Super poder: %f / %f\n", superpoder, superpoder2);
+    if(densidade > densidade2){
+        printf("Na categoria densidade populacional %s venceu", nomec);
+    }
+    else {
+        printf("\nNa categoria densidade populacional %s venceu", nomec2);
+    }
+    if(superpoder > superpoder2){
+        printf("\nNa categoria geral %s venceu",nomec);
+    }
+    else{
+        printf("\nNa categoria geral %s venceu\n", nomec2);
+    }
     return 0;
 }
